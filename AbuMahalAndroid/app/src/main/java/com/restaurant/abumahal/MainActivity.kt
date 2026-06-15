f1bbadfff21f73ad4995a24d16dc10f31258040f
Mohamed <mohamedmonif8@gmail.com>
2026-06-15 15:21:30 +0300

    تحديث الثيم إلى أحمر/أسود وإضافة نظام طباعة الفواتير للمطبخ

---
+++ AbuMahalAndroid/app/src/main/java/com/restaurant/abumahal/MainActivity.kt
@@ -0,0 +1,33 @@
+﻿package com.restaurant.abumahal
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        webView = findViewById(R.id.webView)
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        webView.loadUrl("https://restaurantsystem-g4iu.onrender.com/customer" )
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
+    }
+}
